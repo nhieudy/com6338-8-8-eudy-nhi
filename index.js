f00f36cbaa751b268e9ce57c7b6b393ee59f460b
Nhi Eudy <nhieudy@Mac.attlocal.net>
2025-10-19 23:35:25 -0500

    can search city, and console logs it

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 // Your code here
 var form = document.querySelector('form');
-var URL = 'https://api.openweathermap.org/data/2.5/weather?q=gainesville&units=imperial&appid=61ac71fca852832313e86693bf383076'
+var URL = 'https://api.openweathermap.org/data/2.5/weather?units=imperial&appid=61ac71fca852832313e86693bf383076&q='
 form.onsubmit = function(e){
     e.preventDefault();
-    fetch(URL)
+    var city = this.search.value;
+    fetch(URL + city)
     .then(function(res){
         return res.json()
     })
