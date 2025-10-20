95e8d84942a85f0ce541ef0d2530d66e699a7328
Nhi Eudy <nhieudy@Nhis-MacBook-Pro.local>
2025-10-20 16:13:24 -0500

    Added h2, a, and icon

--- index.js
+++ index.js
@@ -1,14 +1,45 @@
 // Your code here
-var form = document.querySelector('form');
-var URL = 'https://api.openweathermap.org/data/2.5/weather?units=imperial&appid=61ac71fca852832313e86693bf383076&q='
-form.onsubmit = function(e){
-    e.preventDefault();
-    var city = this.search.value;
-    fetch(URL + city)
-    .then(function(res){
-        return res.json()
+var form = document.querySelector("form");
+var section = document.getElementById("weather");
+
+//get the data from open weather map
+var URL =
+  "https://api.openweathermap.org/data/2.5/weather?units=imperial&appid=61ac71fca852832313e86693bf383076&q=";
+form.onsubmit = function (e) {
+  e.preventDefault();
+  var city = this.search.value;
+  fetch(URL + city)
+    .then(function (res) {
+      return res.json();
     })
-    .then(function(data){
-        console.log(data)
-    })
+    .then(function (data) {
+      displayWeather(data);
+      console.log(data);
+    });
+};
+
+function displayWeather(data) {
+  console.log(data.name);
+  console.log(data.sys.country);
+
+  //Create the H2
+  var h2 = document.createElement("h2");
+  h2.textContent = data.name + ", " + data.sys.country;
+  section.appendChild(h2);
+
+  //Create the a href to google maps
+  var a = document.createElement("a");
+  a.textContent = "Click to view map";
+  a.href =
+    "https://www.google.com/maps/search/?api=1&query=" +
+    data.coord.lat +
+    "," +
+    data.coord.lon;
+  section.appendChild(a);
+  form.reset();
+
+  //Create weather icon
+  var icon = document.createElement("img");
+  icon.src = 'https://openweathermap.org/img/wn/' + data.weather[0].icon + '@2x.png';
+  section.appendChild(icon);
 }
