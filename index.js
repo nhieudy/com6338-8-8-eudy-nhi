45e8a3b564892c16cefa6742804d3d90281c50fc
Nhi Eudy <nhieudy@Mac.attlocal.net>
2025-10-19 23:28:01 -0500

    made sure appid works

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code here
+var form = document.querySelector('form');
+var URL = 'https://api.openweathermap.org/data/2.5/weather?q=gainesville&units=imperial&appid=61ac71fca852832313e86693bf383076'
+form.onsubmit = function(e){
+    e.preventDefault();
+    fetch(URL)
+    .then(function(res){
+        return res.json()
+    })
+    .then(function(data){
+        console.log(data)
+    })
+}
